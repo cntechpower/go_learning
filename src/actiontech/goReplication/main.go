5d4516bfcee3d00e14a6fb44459aae83b9ea2193
dujinyang <root@cntechpower.com>
2019-08-27 16:46:55 +0800

    add binlog parser

--- src/actiontech/goReplication/main.go
+++ src/actiontech/goReplication/main.go
@@ -1,48 +1,64 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"os"
 	"strconv"
 
 	uuid "github.com/satori/go.uuid"
 
-	"github.com/siddontang/go-log/log"
-	"github.com/siddontang/go-mysql/mysql"
 	"github.com/siddontang/go-mysql/replication"
 )
 
+type GtidInfo struct {
+	gtid string
+	sql  []string
+}
+
 func main() {
-	cfg := replication.BinlogSyncerConfig{
-		ServerID: 100,
-		Flavor:   "mysql",
-		Host:     "10.186.62.3",
-		Port:     33306,
-		User:     "admin",
-		Password: "admin",
-	}
-	syncer := replication.NewBinlogSyncer(cfg)
-
-	gtidSet, err := mysql.ParseMysqlGTIDSet("35d5c7fa-c7d9-11e9-91a8-cb2d7ff653bb:1-5")
-	if err != nil {
-		log.Fatal("parse mysql gtid set %v error: %v", err)
-	}
-
-	streamer, _ := syncer.StartSyncGTID(gtidSet)
-	for {
-		ev, _ := streamer.GetEvent(context.Background())
-		ev.Dump(os.Stdout)
+	gtidInfos := make([]*GtidInfo, 0)
+	gtidInfo := new(GtidInfo)
+	binlogParser := replication.NewBinlogParser()
+	err := binlogParser.ParseFile("./mysql-bin.000003", 0, func(ev *replication.BinlogEvent) error {
 		switch ev.Event.(type) {
 		case *replication.GTIDEvent:
 			gtidEvent := ev.Event.(*replication.GTIDEvent)
 			uuidInEvent, _ := uuid.FromBytes(gtidEvent.SID)
-			gtid := uuidInEvent.String() + ":" + strconv.FormatInt(gtidEvent.GNO, 10)
-			fmt.Printf("Got GTID :%v\n", gtid)
-		default:
-			fmt.Printf("Other Event\n")
 
+			//append current gtid to list
+			if gtidInfo.gtid != "" {
+				//fmt.Printf("append gtid: %v, sql: %v to list.\n", gtidInfo.gtid, gtidInfo.sql)
+				gtidInfos = append(gtidInfos, gtidInfo)
+			}
+			//clear current gtid and set gtid to next
+			gtidInfo = &GtidInfo{
+				gtid: uuidInEvent.String() + ":" + strconv.FormatInt(gtidEvent.GNO, 10),
+				sql:  nil,
+			}
+		//case *replication.RowsEvent:
+		//	rowsEvent := ev.Event.(*replication.RowsEvent)
+		//	table := string(rowsEvent.Table.Table)
+		//	fmt.Println(table)
+		//	rowsEvent.Dump(os.Stdout)
+		case *replication.RowsQueryEvent:
+			rowsQueryEvent := ev.Event.(*replication.RowsQueryEvent)
+			if gtidInfo.gtid == "" {
+				return fmt.Errorf("found RowQueryEvent without gtid")
+			}
+			gtidInfo.sql = append(gtidInfo.sql, string(rowsQueryEvent.Query))
+
+		default:
+			return nil
 		}
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("parse binlog error %v", err)
+	}
+	//json, _ := json.Marshal(gtidInfos)
+	//fmt.Println(string(json))
+
+	for _, gtid := range gtidInfos {
+		fmt.Printf("GTID is %v, sql is %v\n", gtid.gtid, gtid.sql)
 	}
 
 }
