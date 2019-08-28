5af6362dac268a405d5f47a6bbde65b9bdc6f04b
dujinyang <root@cntechpower.com>
2019-08-28 15:31:04 +0800

    update binlog parser

--- src/actiontech/goReplication/main.go
+++ src/actiontech/goReplication/main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
+	"regexp"
 	"strconv"
+	"strings"
+
+	"github.com/siddontang/go-log/log"
 
 	uuid "github.com/satori/go.uuid"
 
@@ -14,7 +19,7 @@ type GtidInfo struct {
 	sql  []string
 }
 
-func main() {
+func parseBinlog() {
 	gtidInfos := make([]*GtidInfo, 0)
 	gtidInfo := new(GtidInfo)
 	binlogParser := replication.NewBinlogParser()
@@ -26,7 +31,6 @@ func main() {
 
 			//append current gtid to list
 			if gtidInfo.gtid != "" {
-				//fmt.Printf("append gtid: %v, sql: %v to list.\n", gtidInfo.gtid, gtidInfo.sql)
 				gtidInfos = append(gtidInfos, gtidInfo)
 			}
 			//clear current gtid and set gtid to next
@@ -34,11 +38,6 @@ func main() {
 				gtid: uuidInEvent.String() + ":" + strconv.FormatInt(gtidEvent.GNO, 10),
 				sql:  nil,
 			}
-		//case *replication.RowsEvent:
-		//	rowsEvent := ev.Event.(*replication.RowsEvent)
-		//	table := string(rowsEvent.Table.Table)
-		//	fmt.Println(table)
-		//	rowsEvent.Dump(os.Stdout)
 		case *replication.RowsQueryEvent:
 			rowsQueryEvent := ev.Event.(*replication.RowsQueryEvent)
 			if gtidInfo.gtid == "" {
@@ -54,11 +53,66 @@ func main() {
 	if err != nil {
 		fmt.Printf("parse binlog error %v", err)
 	}
-	//json, _ := json.Marshal(gtidInfos)
-	//fmt.Println(string(json))
 
 	for _, gtid := range gtidInfos {
 		fmt.Printf("GTID is %v, sql is %v\n", gtid.gtid, gtid.sql)
+	}
+
+}
+
+func matchMysqlBinlogOutput() error {
+	//Transaction is split by GTID event in mysqlbinlog output, below comment is a GTID event sample
+	//#190827 16:26:19 server id 1174285442  end_log_pos 2784191 CRC32 0x14a911aa 	GTID	last_committed=6242	sequence_number=6243	rbr_only=yes	original_committed_timestamp=0	immediate_commit_timestamp=0	transaction_length=0
+	startOfTransactions := regexp.MustCompile("#.*GTID[[:space:]]*last_committed.*transaction_length=[[:digit:]]+")
+
+	//c95e95e8-c872-11e9-ac89-0242ac148602:39815
+	gtidFormat := regexp.MustCompile("[[:graph:]]{36}:[[:digit:]]+")
+
+	//   ### UPDATE `universe`.`u_delay`
+	//   ### WHERE
+	//   ###   @1='ustats'
+	//   ###   @2='2019-08-27 16:26:17'
+	//   ###   @3=39712
+	//   ### SET
+	//   ###   @1='ustats'
+	//   ###   @2='2019-08-27 16:26:18'
+	//   ###   @3=39713
+	//   # at 2783649
+	//   #190827 16:26:18 server id 1174285442  end_log_pos 2783680 CRC32 0x8e9d69a3     Xid = 1559957
+	//   COMMIT/*!*/
+	startOfSql := regexp.MustCompile("(?s)#{3}.*COMMIT")
+	startOfEvents := regexp.MustCompile("# at [[:digit:]]+")
+	space := regexp.MustCompile("\\s+")
+	cmd := exec.Command("cat", "./mysqlbinlog.out")
+	output, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	trxs := startOfTransactions.Split(string(output), 100)
+	//fmt.Println(trxs[1])
+	for _, trx := range trxs {
+		gtid := gtidFormat.FindStringSubmatch(trx)
+		//match full trx data in sqlsInTrx[0]
+		sqlsInTrx := startOfSql.FindStringSubmatch(trx)
+		if sqlsInTrx != nil {
+			sqls := startOfEvents.Split(sqlsInTrx[0], 100)
+			//sqls[len(sqls)-1] is always be COMMIT, so drop it
+			for _, sql := range sqls[:len(sqls)-1] {
+				fmt.Printf("----------------------------------------------------------------------------------------")
+				//fmt.Printf("\n%v, \n%v\n", gtid[0], strings.Replace(sql, "#", "", 100))
+				sqlNew := space.ReplaceAllString(strings.Replace(sql, "#", "", 100), " ")
+				fmt.Printf("\n%v, \n%v\n", gtid[0], sqlNew)
+			}
+		}
+	}
+	return nil
+
+}
+
+func main() {
+	//parseBinlog()
+	if err := matchMysqlBinlogOutput(); err != nil {
+		log.Fatal(err.Error())
 	}
 
 }
