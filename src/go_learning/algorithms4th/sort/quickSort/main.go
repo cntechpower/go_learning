db973882d9bc1c81384bbd0bedb0ea37a8078f3f
dujinyang <root@cntechpower.com>
2020-06-16 01:05:49 +0800

    fix quick sort bug

--- src/go_learning/algorithms4th/sort/quickSort/main.go
+++ src/go_learning/algorithms4th/sort/quickSort/main.go
@@ -1,76 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"go_learning/algorithms4th/sort/testUtil"
-	"math/rand"
-	"time"
 )
 
-func NewRandomList(length int) []int {
-	res := make([]int, length)
-	for i := 0; i < length; i++ {
-		res[i] = rand.New(rand.NewSource(time.Now().UnixNano())).Int()
-	}
-	return res
-}
-func IsSorted(list []int, isAsc bool) bool {
-	for i := 1; i < len(list); i++ {
-		if (isAsc && list[i-1] > list[i]) || (!isAsc && list[i-1] < list[i]) {
-			return false
-		}
-	}
-	return true
-}
-
-func print(list []int) {
-	fmt.Println("-------------------------------------")
-	for _, p := range list {
-		fmt.Println(p)
-	}
-	fmt.Println("-------------------------------------")
-}
-
-func partition(list []int, lo, hi int, isAsc bool) int {
-	i := lo
-	j := hi
-	v := list[lo]
-	for {
-		for (isAsc && list[i] < v) || (!isAsc && list[i] > v) {
+func partition(list []int, start, end int, isAsc bool) int {
+	i := start
+	pivot := list[end]
+	for j := start; j < end; j++ {
+		if (isAsc && list[j] < pivot) || (!isAsc && list[j] > pivot) {
+			if i != j {
+				list[i], list[j] = list[j], list[i]
+			}
 			i++
-			if i == hi {
-				break
-			}
 		}
-		for (isAsc && list[j] >= v) || (!isAsc && list[j] <= v) {
-			j--
-			if j == lo {
-				break
-			}
-		}
-		if i >= j {
-			break
-		}
-		fmt.Printf("will swap i=%v, j=%v\n", i, j)
-		tmp := list[i]
-		list[i] = list[j]
-		list[j] = tmp
 	}
-	//TODO: Why need this?  P184
-	//fmt.Printf("will swap i=%v, j=%v\n", lo, j)
-	//tmp := list[lo]
-	//list[lo] = list[j]
-	//list[j] = tmp
-	return j
+	list[i], list[end] = list[end], list[i]
+	return i
 }
 
-func sort(list []int, lo, hi int, isAsc bool) {
-	if hi <= lo {
+func sort(list []int, start, end int, isAsc bool) {
+	if end <= start {
 		return
 	}
-	j := partition(list, lo, hi, isAsc)
-	sort(list, lo, j-1, isAsc)
-	sort(list, j+1, hi, isAsc)
+	j := partition(list, start, end, isAsc)
+	sort(list, start, j-1, isAsc)
+	sort(list, j+1, end, isAsc)
 }
 
 func quickSort(list []int, isAsc bool) {
